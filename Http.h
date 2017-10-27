b8f126ed7da0b0cfb2baee70f104097bfdfd7ae7
terminalobject <doctorboredom@hotmail.com>
2017-10-27 23:34:25 +0100

    first commit, project setup

---
+++ Http.h
@@ -0,0 +1,13 @@
+#ifndef Http_h
+#define Http_h
+
+#include <string>
+
+class Http {
+public:
+   virtual ~Http() {}
+   virtual void initialize() = 0;
+   virtual std::string get(const std::string& url) const = 0;
+};
+
+#endif
